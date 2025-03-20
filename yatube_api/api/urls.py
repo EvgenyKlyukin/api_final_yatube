2e21ac06119d89a6acb6c2533684a4bbeb7b64aa
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-20 20:43:06 +0500

    Подключите эндпоинты к проекту
    Согласно документации ReDoc были настроены эндпоинты в api/urls.py для: 'posts/', 'posts/{post_id}/comments'. Созданы viewset'ы для работы с моделями Post и Comment.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,26 @@
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from yatube_api.api.views import CommentViewSet, PostViewSet
+
+router_v1 = SimpleRouter()
+
+router_v1.register(
+    'posts',
+    PostViewSet,
+    basename='posts')
+router_v1.register(
+    r'^posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+v1_urlpatterns = [
+    path('auth/', include('djoser.urls')),  # Для управления пользователями.
+    path('jwt/', include('djoser.urls.jwt')),  # JWT-эндпоинты.
+    path('', include(router_v1.urls)),
+]
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),  # Для управления пользователями.
-    path('auth/', include('djoser.urls.jwt')),  # JWT-эндпоинты.
+    path('v1/', include(v1_urlpatterns)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,14 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+
+from yatube_api.api.serializers import CommentSerializer, PostSerializer
+from yatube_api.posts.models import Comment, Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
