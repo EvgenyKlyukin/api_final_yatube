0eec9e0903c6618c49a6a449230398463d7dd538
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-20 19:30:29 +0500

    Подключите и настройте JWT-аутентификацию
    По условиям ТЗ необходимо использовать для аутентификации JWT-токены, поэтому было проделано следующее.
    
    Подключены две библиотеки Djoser и Simple JWT. Обновлены INSTALLED_APPS и REST_FRAMEWORK в settings.py. Добавлена переменная SIMPLE_JWT в settings.py.  Добавлены url в api/urls.py.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),  # Для управления пользователями.
+    path('auth/', include('djoser.urls.jwt')),  # JWT-эндпоинты.
 ]
