a5e89d6262d6b1fcdb45384fecf70133a70e55fe
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-22 15:27:56 +0500

    feat(api/urls.py): add follow/ urls
    An endpoint `follow/` was added

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,23 +1,19 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
-from api.views import CommentViewSet, GroupsViewSet, PostViewSet
+from api.views import (CommentViewSet, FollowViewSet,
+                       GroupsViewSet, PostViewSet)
 
-router_v1 = SimpleRouter()
+router_v1 = DefaultRouter()
 
-router_v1.register(
-    'posts',
-    PostViewSet,
-    basename='posts')
-router_v1.register(
-    'groups',
-    GroupsViewSet,
-    basename='groups')
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('groups', GroupsViewSet, basename='groups')
 router_v1.register(
     r'^posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
+router_v1.register(r'follow', FollowViewSet, basename='follow')
 
 v1_urlpatterns = [
     path('', include('djoser.urls.jwt')),  # JWT-эндпоинты.
