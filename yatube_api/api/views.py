4918fe77520a7ceb440026f83d2e878888f370c6
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-22 07:04:45 +0500

    feat(constants,py, views.py): add constants.py and method perform_create
    Added constants.py for constants. Added the `perform_create` method to `PostViewSet`. It is necessary to set the current user as the author of the post.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,12 +1,17 @@
 from rest_framework import viewsets
 
 from api.serializers import CommentSerializer, PostSerializer
+from api.permissions import IsAuthorOrReadOnly
 from posts.models import Comment, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
