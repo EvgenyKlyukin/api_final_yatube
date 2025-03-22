691a83405ca7e604fa86c74772ca908ca1ca833f
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-22 07:29:38 +0500

    feat(views.py): add methods for the CommentViewSet model
    add methods:
    get_post - Returns the post to which the comments relate.
    get_queryset - Returns comments belonging to the specified post
    perform_create - Saves a new comment by identifying the current user as the author and linking it to the post.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
 from api.serializers import CommentSerializer, PostSerializer
@@ -6,14 +7,35 @@ from posts.models import Comment, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
+    """ViewSet для работы с постами."""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
+        """
+        Сохраняет новый пост, устанавливая автором текущего
+        пользователя.
+        """
         serializer.save(author=self.request.user)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSet для работы с комментариями."""
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_post(self):
+        """Возвращает пост, к которому относятся комментарии."""
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        """Возвращает комментарии, принадлежащие указанному посту."""
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        """
+        Сохраняет новый комментарий, устанавливая автором текущего
+        пользователя и связывая его с постом.
+        """
+        serializer.save(author=self.request.user, post=self.get_post())
