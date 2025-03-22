7b94087ed62aa26fbaf84f6d055dbf62f5d9d409
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-22 09:43:16 +0500

    refactor(permissions.py): update custom permission IsAuthorOrReadOnly
    According to the task requirements, authenticated users are allowed to modify and delete their own content; in all other cases, access is granted for reading only. For convenience, it was decided to modify the permission and add an authentication check for the user. The permission is planned to be used at the project level.

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,15 +5,25 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
     Кастомное разрешение, позволяющее только автору объекта выполнять запросы
     на изменение.
-
-    Разрешение проверяет, является ли пользователь автором объекта.
-    Если пользователь не является автором, то разрешены только безопасные
-    методы (GET, HEAD, OPTIONS).
     """
 
     message = 'У вас недостаточно прав для выполнения данного действия.'
 
+    def has_permission(self, request, view):
+        """
+        Разрешение проверяет подлинность пользователя. Если пользователь не
+        аутентифицирован, то доступны только безопасные методы.
+        """
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
+        )
+
     def has_object_permission(self, request, view, obj):
-        """Проверяет, является ли пользователь автором объекта."""
+        """
+        Если пользователь не является автором, то разрешены только безопасные
+        методы.
+        """
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
