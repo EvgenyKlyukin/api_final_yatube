5d41dfdc27e2c16334816050832a419c783110a5
Evgeny Klyukin <key.klyukin@gmail.com>
2025-03-24 06:53:32 +0500

    feat(admin.py): add display of models in the admin panel
    add displaying models from models.py in the admin panel

--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Group, Post, Comment, Follow
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    search_fields = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Group, GroupAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author', 'group')
+    search_fields = ('text', 'author__username')
+    list_filter = ('pub_date', 'author', 'group')
+    ordering = ('-pub_date',)
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created')
+    search_fields = ('author__username', 'post__text')
+    list_filter = ('created', 'post')
+
+
+admin.site.register(Comment, CommentAdmin)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'following')
+    search_fields = ('user__username', 'following__username')
+    list_filter = ('user', 'following')
+
+
+admin.site.register(Follow, FollowAdmin)
